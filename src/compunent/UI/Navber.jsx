46d3dcccc9eb6451085c989ec056011ff5fa8713
engr-mdalrafi <engr.mdalrafi@gmail.com>
2025-12-05 23:31:40 +0600

    navbar edit

--- src/compunent/UI/Navber.jsx
+++ src/compunent/UI/Navber.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 const Navber = () => {
   return (
     <>
-      
+      <nav className='bg-amber-300'>
+        <div className="container">
+          <div className='py-11 flex justify-between items-center'>
+            <div><a href="#"><h2>Al Rafi</h2></a></div>
+            <div>
+              <ul className='flex gap-3'>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Fatures</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Fesume</a></li>
+                <li><a href="#">Clients</a></li>
+                <li><a href="#">Pricinc</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contract</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
     </>
   )
 }
